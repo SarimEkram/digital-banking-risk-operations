95a01720feb10bbab1c4728ee05ef1ba3a8e50c8
Sarim Ekram <sarim.ekram1@ucalgary.ca>
2026-02-06 05:49:17 -0700

    added find user by id order by desc

--- src/main/java/com/sarim/digitalbanking/accounts/AccountRepository.java
+++ src/main/java/com/sarim/digitalbanking/accounts/AccountRepository.java
@@ -1,6 +1,8 @@
 package com.sarim.digitalbanking.accounts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface AccountRepository extends JpaRepository<AccountEntity, Long> {
+    List<AccountEntity> findByUserIdOrderByIdAsc(Long userId);
 }
