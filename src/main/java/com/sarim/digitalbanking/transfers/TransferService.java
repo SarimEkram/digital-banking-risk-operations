666a1fa1f92a41fd5ae0f521059e81c787f11122
Sarim Ekram <sarim.ekram1@ucalgary.ca>
2026-02-21 19:47:25 -0700

    removed a comment

--- src/main/java/com/sarim/digitalbanking/transfers/TransferService.java
+++ src/main/java/com/sarim/digitalbanking/transfers/TransferService.java
@@ -32,7 +32,7 @@ import java.util.List;
 @Service
 public class TransferService {
 
-    // all variabless
+
     private final AccountRepository accountRepository;
     private final TransferRepository transferRepository;
     private final LedgerEntryRepository ledgerEntryRepository;
